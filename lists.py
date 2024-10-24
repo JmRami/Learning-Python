3ad80ec549d1796e5b44e406d2f47f5345d92146
JuanMa <jmramirez6767@gmail.com>
2024-10-24 15:23:06 +0200

    First commit

---
+++ lists.py
@@ -0,0 +1,18 @@
+my_list = list()
+
+print(len(my_list))
+my_list.append(12)
+
+my_list = ["p1",  "p2", "p3"]
+
+print(my_list)
+print(my_list.count("p1"))
+
+v1, v2, v3 = my_list
+
+print(v1)
+print(v2)
+print(v3)
+print(v3)
+
+print("He creado una nueva linea")
